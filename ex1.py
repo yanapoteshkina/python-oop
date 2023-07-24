cb2737dcc16d2abb9f94f5b8d480e1851a091083
yanap <tatiyanavoron19@gmail.com>
2023-07-24 19:11:17 +0600

    add ex

---
+++ ex1.py
@@ -0,0 +1,25 @@
+class House:
+#Атрибуты/свойства класса
+     theme = 'Color mode'
+     author = 'Name '
+     city = 'City'
+
+#Создание экземпляра класса
+house1 = House()
+house2 = House()
+
+#Через свойство экземпляр класса, назначаем новое свойство класа, создавая в просранстве экземпляра значение
+house1.theme = 'pink mode'
+house1.theme = 'dark mode'
+
+# Если атрибута не сущсетвует, то он создается, можно также для этого использовать setattr
+# setattr(House, styleHome, 'Korean')
+House.styleHome = 'Korean'
+
+#getattr(obj, name, [default]) - возвращает значение атрибута обьекта
+#hasattr(obj, name) - проверяет на наличие атрибута name в obj
+#setattr(obj, name, value) - задает значение атрибута (если атрибута не сущсетвует, то он создается)
+#delattr(obj, name) - удаляет атрибут с именем name
+
+#.__doc__ - содержит строку с описанием класса
+#.__dict__ - содержит набор атрибутов экземпляра класса
